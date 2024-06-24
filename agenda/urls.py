bfa713495aab705ad020cbb63bacac71ba275f2d
Vvargasl <larissavargaspimentel@gmail.com>
2024-06-24 20:01:50 -0300

    listagem agendaDjango

--- agenda/urls.py
+++ agenda/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('agenda/',views.lista_eventos),
+    path('', RedirectView.as_view(url='/agenda/'))
+    # path('', views.index)  -->> 1ª MANEIRA DE REDIRECIONAR O INDEX
 ]
