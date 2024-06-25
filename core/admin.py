a9fc1941ed9838f187e9b80781c49b3f27c515c9
Vvargasl <larissavargaspimentel@gmail.com>
2024-06-25 18:00:44 -0300

    Incluido Funcoes de alteracao e exclusao de dados

--- core/admin.py
+++ core/admin.py
@@ -5,7 +5,7 @@ from core.models import Evento
 
 
 class EventoAdmin(admin.ModelAdmin):
-    list_display = ("titulo","data_evento","data_criacao")
+    list_display = ("id","titulo","data_evento","data_criacao")
     list_filter = ("titulo","usuario","data_evento",)
 
 
