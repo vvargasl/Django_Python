52cc38d1e58df66eb758ab8f0f8b33e3bc120c8d
Vvargasl <larissavargaspimentel@gmail.com>
2024-06-25 18:56:00 -0300

    Adicionado json, http404 e conf timezone

--- core/views.py
+++ core/views.py
@@ -3,6 +3,8 @@ from core.models import Evento
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from datetime import datetime, timedelta
+from django.http.response import Http404, JsonResponse
 # Create your views here.
 
 # def index (request):  -->> 1ª MANEIRA DE REDIRECIONAR O INDEX
@@ -31,7 +33,9 @@ def submit_login(request):
 @login_required(login_url='/login/')
 def lista_eventos(request):
     usuario= request.user
+    #data_atual = datetime.now() - timedelta(hours=1) --> se eu quiser filtrar so eventos da data atual
     evento = Evento.objects.filter(usuario=usuario)
+                                   #,data_evento__gt=data_atual) --> filtrar so data atual ^^
     dados = {'eventos':evento}
     return render(request,'agenda.html',dados)
 
@@ -71,8 +75,22 @@ def submit_evento(request):
 @login_required(login_url='/login/')
 def delete_evento(request, id_evento):
     usuario=request.user
-    evento = Evento.objects.get(id=id_evento)
+
+    try:
+        evento = Evento.objects.get(id=id_evento)
+    except Exception:
+        raise Http404()
+
     if usuario == evento.usuario:
         evento.delete()
 
+    else:
+        raise Http404()
+
     return redirect('/')
+
+@login_required(login_url='/login/')
+def json_lista_evento(request):
+    usuario = request.user
+    evento = Evento.objects.filter(usuario=usuario).values('id', 'titulo')
+    return JsonResponse(list(evento), safe=False)
